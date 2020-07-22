87a2778bc407e9bb0bac1b21535baa07b17b12c9
Nikhil Sain <59610773+Nikhilsain@users.noreply.github.com>
2020-07-22 19:47:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -322,6 +322,6 @@ function isLoggedIn(req,res,next){
 }
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,process.env.IP,function(){
     console.log("this server strats");
 });
